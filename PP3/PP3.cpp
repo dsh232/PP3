4ab8a9686f56af75b4396091cec9ba403ce389d9
dsh232 <nifitca.lex@gmail.com>
2024-10-30 02:09:41 +0400

    done

--- PP3/PP3.cpp
+++ PP3/PP3.cpp
@@ -4,14 +4,22 @@
 #include <algorithm>
 #include <chrono>
 
-
-int int_input()		//проверка на ввод.
-{
+int int_input() {
     int input;
-    while (!(std::cin >> input) //соответсвие введенного значения типу переменной
-        || std::cin.peek() != '\n')  //проверка на конец ввода
+    while (!(std::cin >> input) || std::cin.peek() != '\n') {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Некорректный ввод. Повторите снова. " << std::endl;
+    }
+    return input;
+}
+
+char char_input()
+{
+    char input;
+    while (!(std::cin >> input) || std::cin.peek() != '\n')
     {
-        std::cin.clear();	//"лечим" поток. +состояние good
+        std::cin.clear();
         while (std::cin.get() != '\n');
         std::cout << "Некорректный ввод. Повторите снова. " << std::endl;
     }
@@ -24,7 +32,6 @@ void insertionSort(std::vector<int>& arr) {
         int key = arr[i];
         int j = i - 1;
 
-        // Перемещаем элементы arr[0..i-1], которые больше key, на одну позицию вперед
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
@@ -34,25 +41,20 @@ void insertionSort(std::vector<int>& arr) {
 }
 
 void heapify(std::vector<int>& arr, int n, int i) {
-    int largest = i; // Инициализируем наибольший элемент как корень
-    int left = 2 * i + 1; // Левый дочерний элемент
-    int right = 2 * i + 2; // Правый дочерний элемент
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-    // Если левый дочерний элемент больше корня
     if (left < n && arr[left] > arr[largest]) {
         largest = left;
     }
 
-    // Если правый дочерний элемент больше текущего наибольшего
     if (right < n && arr[right] > arr[largest]) {
         largest = right;
     }
 
-    // Если наибольший элемент не корень
     if (largest != i) {
-        std::swap(arr[i], arr[largest]); // Меняем местами
-
-        // Рекурсивно "просеиваем" затронутое поддерево
+        std::swap(arr[i], arr[largest]);
         heapify(arr, n, largest);
     }
 }
@@ -60,29 +62,26 @@ void heapify(std::vector<int>& arr, int n, int i) {
 void heapSort(std::vector<int>& arr) {
     int n = arr.size();
 
-    // Построение кучи (перегруппировка массива)
     for (int i = n / 2 - 1; i >= 0; i--) {
         heapify(arr, n, i);
     }
 
-    // Один за другим извлекаем элементы из кучи
     for (int i = n - 1; i > 0; i--) {
-        std::swap(arr[0], arr[i]); // Перемещаем текущий корень в конец
-        heapify(arr, i, 0); // Вызываем heapify на уменьшенной куче
+        std::swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
     }
 }
 
-void printArray(std::vector<int>& arr)
-{
-    for (int i = 0; i < arr.size(); ++i)
-        std::cout << arr[i] << " ";
+void printArray(const std::vector<int>& arr) {
+    for (const int& value : arr)
+        std::cout << value << " ";
     std::cout << "\n";
 }
 
 std::vector<int> createRandomArray(int n) {
     std::vector<int> newVector;
     for (int i = 0; i < n; i++) {
-        newVector.push_back(round(rand() % 100));
+        newVector.push_back(rand() % 100); // Генерация случайных чисел от 0 до 99
     }
     return newVector;
 }
@@ -90,7 +89,7 @@ std::vector<int> createRandomArray(int n) {
 std::vector<int> createAlmostRandomArray(int n) {
     std::vector<int> newVector;
     for (int i = 0; i < n; i++) {
-        newVector.push_back(round(rand() % 100) * (i+1));
+        newVector.push_back(rand() % 100 * (i + 1)); // Почти отсортированные данные
     }
     return newVector;
 }
@@ -98,63 +97,110 @@ std::vector<int> createAlmostRandomArray(int n) {
 std::vector<int> createManualArray(int n) {
     std::vector<int> newVector;
     for (int i = 0; i < n; i++) {
-        std::cout << "Введите " << i << " элемент массива: ";
+        std::cout << "Введите " << i + 1 << " элемент массива: "; 
         newVector.push_back(int_input());
     }
     return newVector;
 }
 
+void howCreateArray(std::vector<int>& vec, int arr_size) {
+    int answer;
+    std::cout << "Как бы вы хотели заполнить данные в вектор? \n1 - ручной ввод,\n2 - случайные данные,\n3 - 'почти отсортированные' случайные данные." << std::endl;
+
+    answer = int_input();
+    std::cout << std::endl;
+
+    if (answer == 1) {
+        vec = createManualArray(arr_size); // Сохраняем результат
+    }
+    else if (answer == 2) {
+        vec = createRandomArray(arr_size); // Сохраняем результат
+    }
+    else if (answer == 3) {
+        vec = createAlmostRandomArray(arr_size); // Сохраняем результат
+    }
+    else {
+        std::cout << "Некорректный ввод. Введите цифры 1 или 2 или 3\n";
+        howCreateArray(vec, arr_size); // Повторный вызов для корректного ввода
+    }
+}
 
 int main() {
     setlocale(LC_ALL, "ru");
-    srand(static_cast<unsigned int>(time(0)));
-    int n;
-    std::cout << "Введите размер массива: ";
-    n = int_input();
-    std::cout << std::endl;
+    bool is_need_next_run = false; //создание флага для повторного запуска программы
+    while (is_need_next_run == false) {
+        srand(static_cast<unsigned int>(time(0)));
 
-    std::vector<int> sortArr = createRandomArray(n);
-    std::vector<int> piramidArr = sortArr;
-    std::vector<int> insertArr = sortArr;
+        int arr_size;
 
-    // Выводим исходный массив
-    std::cout << "Исходный массив: ";
-    printArray(piramidArr);
+        std::cout << "Введите размер массива: ";
+        arr_size = int_input();
 
-    auto start = std::chrono::high_resolution_clock::now(); // Начало отсчета времени
-    insertionSort(insertArr);
-    auto end = std::chrono::high_resolution_clock::now(); // Конец отсчета времени
-    std::chrono::duration<double> insert_arr_duration = end - start;
+        std::vector<int> sortArr; // Инициализация вектора для хранения данных
+        howCreateArray(sortArr, arr_size); // Заполнение вектора
 
-    std::cout << "Сортировка вставками: ";
-    printArray(insertArr);
+        // Выводим исходный массив
+        std::cout << "Исходный массив: ";
+        printArray(sortArr);
 
-    start = std::chrono::high_resolution_clock::now(); // Начало отсчета времени
-    heapSort(piramidArr);
-    end = std::chrono::high_resolution_clock::now(); // Конец отсчета времени
-    std::chrono::duration<double> piramid_arr_duration = end - start;
-    
-    std::cout << "Пирамидальная сортировка: ";
-    printArray(piramidArr);
-    
-    start = std::chrono::high_resolution_clock::now(); // Начало отсчета времени
-    std::sort(sortArr.begin(), sortArr.end());
-    end = std::chrono::high_resolution_clock::now(); // Конец отсчета времени
-    std::chrono::duration<double> std_sort_duration = end - start;
+        // Копируем массив для различных сортировок
+        auto insertArr = sortArr;
+        auto piramidArr = sortArr;
 
-    std::cout << "Стандартная сортировка: ";
-    printArray(sortArr);
+        auto start = std::chrono::high_resolution_clock::now(); // Начало отсчета времени
+        insertionSort(insertArr);
+        auto end = std::chrono::high_resolution_clock::now(); // Конец отсчета времени
+        std::chrono::duration<double> insert_arr_duration = end - start;
+
+        std::cout << "Сортировка вставками: ";
+        printArray(insertArr);
 
 
-    std::cout << "\nВремя выполнения стандартной сортировки: " << std_sort_duration.count() << " секунд.";
-    std::cout << "\nВремя выполнения сортировки вставками: " << insert_arr_duration.count() << " секунд.";
-    std::cout << "\nВремя выполнения пирамидальной сортировки: " << piramid_arr_duration.count() << " секунд." << std::endl;
+        start = std::chrono::high_resolution_clock::now(); // Начало отсчета времени
+        heapSort(piramidArr);
+        end = std::chrono::high_resolution_clock::now(); // Конец отсчета времени
+        std::chrono::duration<double> piramid_arr_duration = end - start;
 
-    std::cout << "\nУскорение стандартной сортировки по сравнению с сортировкой вставками: " << insert_arr_duration.count() / std_sort_duration.count();
-    std::cout << "\nУскорение стандартной сортировки по сравнению с пирамидальной сортировкой: " << piramid_arr_duration.count() / std_sort_duration.count() << std::endl;
-    
+        std::cout << "Пирамидальная сортировка: ";
+        printArray(piramidArr);
 
-    piramidArr.clear();
-    insertArr.clear();
-    sortArr.clear();
+        start = std::chrono::high_resolution_clock::now(); // Начало отсчета времени
+        std::sort(sortArr.begin(), sortArr.end());
+        end = std::chrono::high_resolution_clock::now(); // Конец отсчета времени
+        std::chrono::duration<double> std_sort_duration = end - start;
+
+        std::cout << "Стандартная сортировка: ";
+        printArray(sortArr);
+
+        // Вывод времени выполнения
+        std::cout << "\nВремя выполнения стандартной сортировки: " << std_sort_duration.count() << " секунд.";
+        std::cout << "\nВремя выполнения сортировки вставками: " << insert_arr_duration.count() << " секунд.";
+        std::cout << "\nВремя выполнения пирамидальной сортировки: " << piramid_arr_duration.count() << " секунд." << std::endl;
+
+        // Ускорение стандартной сортировки по сравнению с другими методами
+        if (insert_arr_duration.count() > 0)
+            std::cout << "\nУскорение стандартной сортировки по сравнению с сортировкой вставками: "
+            << insert_arr_duration.count() / std_sort_duration.count();
+
+        if (piramid_arr_duration.count() > 0)
+            std::cout << "\nУскорение стандартной сортировки по сравнению с пирамидальной сортировкой: "
+            << piramid_arr_duration.count() / std_sort_duration.count() << std::endl;
+
+        
+        bool is_need_processed = false;
+        do {
+            std::cout << "Хотите продолжить? (Y/N):  ";
+            char answer = char_input();
+            if (answer == 'N') {
+                std::cout << "Пока!" << std::endl;
+                is_need_next_run = true;
+                is_need_processed = true;
+            }
+            else if (answer == 'Y') {
+                std::cout << "Продолжить? конечно!" << std::endl;
+                is_need_processed = true;
+            }
+
+        } while (is_need_processed == false);
+    }
 }
